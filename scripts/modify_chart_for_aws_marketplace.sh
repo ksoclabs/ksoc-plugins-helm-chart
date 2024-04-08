0d2ccc41806084de511c2e035c0e2f1ee4042bd5
Max Brain <max.brain@ksoc.com>
2024-04-08 14:24:58 +0600

    Adds empty deployment for EKS
    Signed-off-by: Max Brain <max.brain@ksoc.com>

--- scripts/modify_chart_for_aws_marketplace.sh
+++ scripts/modify_chart_for_aws_marketplace.sh
@@ -3,11 +3,14 @@
 FILE="./stable/ksoc-plugins/values.yaml"
 
 GCR_REGISTRY_NAME="us.gcr.io/ksoc-public"
-
 FALCO_REGISTRY="docker.io/falcosecurity"
+ECR_PUBLIC_REGISTRY="public.ecr.aws/eks-distro/kubernetes"
 
 ECR_REGISTRY_NAME="709825985650.dkr.ecr.us-east-1.amazonaws.com/ksoc-labs"
 
+sed -i "s|$ECR_PUBLIC_REGISTRY|$ECR_REGISTRY_NAME|g" "$FILE"
 sed -i "s|$GCR_REGISTRY_NAME|$ECR_REGISTRY_NAME|g" "$FILE"
 sed -i "s|$FALCO_REGISTRY|$ECR_REGISTRY_NAME|g" "$FILE"
 sed -i '/# --/d' "$FILE"
+
+yq e -i '.eksAddon.enabled = true' $FILE
